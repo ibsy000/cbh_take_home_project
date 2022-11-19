fb9e6176c6b913f48ece00d40eea145cf68719bd
Ibsy Matute <ibsy@rocketmail.com>
2022-11-18 18:38:18 -0600

    added spaces for more readable formatting

--- dpk.js
+++ dpk.js
@@ -5,14 +5,19 @@ exports.deterministicPartitionKey = (event) => {
     
     // if there is an event
     if (event) {
+
         // if the event has a partitionKey
         if (event.partitionKey) {
+
             // the candidate equals the event's paritionKey
             candidate = event.partitionKey;
+
             // if the event does not have a partitionKey
         } else {
+
             // create a variable data to convert the event into a string
             const data = JSON.stringify(event);
+            
             // the candidate now equals a created hash instance that updates
             // the hash content using the update() function, to get the hash value
             // we use digest
@@ -22,12 +27,15 @@ exports.deterministicPartitionKey = (event) => {
     
     // there is a candidate value
     if (candidate) {
+
         // if the datatype of candidate is not a string
         if (typeof candidate !== "string") {
             // the candidate will equal a stringified version of candidate
             candidate = JSON.stringify(candidate);
         }
+
         const MAX_PARTITION_KEY_LENGTH = 256;
+
         // if the candidate's value's length is greater than the max_partition_key_length 
         if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
             // the candidate now equals a created hash instance that updates
@@ -35,6 +43,7 @@ exports.deterministicPartitionKey = (event) => {
             // we use digest
             candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");;
         }
+
     // if there is no candidate value
     } else {
         // the candidate will equal the trivial_partition_key
